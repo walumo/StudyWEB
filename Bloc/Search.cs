25dd5cb0a5e3d484ddaac5c116b0533b36b0ba37
walumo <miikka.hakulinen@awacademy.training>
2022-07-20 16:36:58 +0300

    Show all topics refactored with accordion and view working

---
+++ Bloc/Search.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using StudyWEB.Models;
+
+namespace StudyDiary
+{
+    static class Search
+    {
+        public static void Topic()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("SEARCH:\n");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.Write("Search for topic by id or name (leave blank return): ");
+
+                string input = Console.ReadLine();
+                Console.Clear();
+
+                using (StudyDiaryContext db = new StudyDiaryContext())
+                {
+                    if (String.IsNullOrWhiteSpace(input)) break;
+                    else if (input.StartsWith('#')) Load.SmartSearch(input);
+                    else if (!String.IsNullOrWhiteSpace(input) && int.TryParse(input, out int result)) Load.GetTopics(result);
+                    else if (!String.IsNullOrWhiteSpace(input)) Load.GetTopics(input);
+                } 
+            }
+        }
+    }
+}
