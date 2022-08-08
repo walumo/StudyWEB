5b1fbbff882ab598f880d38ffe97a3bf3ecb9c24
walumo <miikka.hakulinen@awacademy.training>
2022-08-08 16:55:08 +0300

    Readable validation errors for topic form

--- Models/Topic.cs
+++ Models/Topic.cs
@@ -15,26 +15,28 @@ namespace StudyWEB.Models
 
         public int TopicId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Topic title can not be empty!")]
         [StringLength(64)]
         public string TopicTitle { get; set; }
         
         [StringLength(256)]
         public string TopicDescription { get; set; }
         
+        [Required(ErrorMessage = "Estimated time can not be empty!")]
         public double TopicEstimatedTimeToMaster { get; set; }
         
         public double? TopicTimeSpent { get; set; }
         
         [StringLength(256)]
         public string TopicSource { get; set; }
-        
+
         public DateTime? TopicStartLearningDate { get; set; }
         
         public bool TopicInProgress { get; set; }
         
         public bool TopicIsDone { get; set; }
-        
+
+        [Required(ErrorMessage = "Enter deadline for this topic!")]
         public DateTime TopicCompletionDate { get; set; }
 
         public virtual ICollection<Task> Tasks { get; set; }
